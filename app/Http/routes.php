dbd2bc78f5e5bf4a3322353e8092ae5c858333d1
mera9588 <morodriguezar@est.utn.ac.cr>
2016-03-18 20:52:37 -0600

    Vista de Registro
    Se modifico el HTML del registro y se agrego la ruta. Quedo pendiente
    enviar los datos a la BD

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,4 +31,6 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('auth/login', 'Auth\AuthController@getLogin');
     Route::post('auth/login', 'Auth\AuthController@postLogin');
     Route::get('auth/logout', 'Auth\AuthController@getLogout');
+    Route::get('auth/register', 'Auth\AuthController@getRegister');
+    Route::post('auth/register', 'Auth\AuthController@postRegister');
 });
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <html lang="es">
 
 <head>
-  <title>Proyecto WebMail</title>
+  <title>Registro de Usuario</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0">
   <link rel="stylesheet" href="/bootstrap.min.css">
@@ -11,26 +11,15 @@
 </head>
 
 <body>
-  <!-- Muestra los errrores del login. -->
-  @if (count($errors) > 0)
-  <div class="alert alert-danger">
-    <ul>
-      @foreach ($errors->all() as $error)
-      <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  </div>
-  @endif
-
   <div class="container" style="margin-top:40px">
     <div class="row">
       <div class="col-sm-6 col-md-4 col-md-offset-4">
         <div class="panel panel-default">
           <div class="panel-heading">
-            <strong> Proyecto Web Mail </strong>
+            <strong> Registro de Usuario </strong>
           </div>
           <div class="panel-body">
-            <form role="form" action="/auth/login" method="POST">
+            <form role="form" method="post" action="/auth/register">
               {!! csrf_field() !!}
               <fieldset>
                 <div class="row">
@@ -40,9 +29,25 @@
                         <span class="input-group-addon">
                           <i class="glyphicon glyphicon-user"></i>
                         </span> 
-                        <input class="form-control" placeholder="Email" name="email" type="email" id="loginname" autofocus required/>
+                        <input class="form-control" placeholder="Nombre" name="name" type="text" id="name" autofocus required/>
                       </div>
                     </div>
+                    <div class="form-group">
+                      <div class="input-group">
+                        <span class="input-group-addon">
+                          <i class="glyphicon glyphicon-user"></i>
+                        </span> 
+                        <input class="form-control" placeholder="Apellido" name="lastname" type="text" id="lastname" required/>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="input-group">
+                        <span class="input-group-addon">
+                          <i class="glyphicon glyphicon-user"></i>
+                        </span> 
+                        <input class="form-control" placeholder="Email" name="email" type="email" id="email" required/>
+                      </div>
+                    </div>  
                     <div class="form-group">
                       <div class="input-group">
                         <span class="input-group-addon">
@@ -52,22 +57,23 @@
                       </div>
                     </div>
                     <div class="form-group">
-                      <input type="submit" id="btnLogin" class="btn btn-lg btn-primary btn-block" value="Iniciar Sesión">
+                      <input id="saveClient" type="submit" value="Registrarse" class="btn btn-primary btn-block"/>
+                    </div>
+                    <div>
+                      <input type="button" class="btn btn-secundary pull-rigth btn-block" value="Cancelar" onClick="location.href='login'">
                     </div>
                   </div>
                 </div>
               </fieldset>
             </form>
-          </div>
-          <div class="panel-footer">
-            No tienes cuenta! <a href="#" onClick="location.href='register'"> Registrate aquí </a>
-          </div>
+          </div>  
         </div>
       </div>
     </div>
   </div>
 
-  <script src="/js/jquery.js"></script>
-  <script src="/js/bootstrap.min.js"></script>
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>	
